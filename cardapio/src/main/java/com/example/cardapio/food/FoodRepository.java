04457295571129cb5ed34c55ca5dcb9dee70353a
Kayky Bruno <109919604+kaykybarbosa@users.noreply.github.com>
2023-05-07 16:55:40 -0300

    Add findById.

--- cardapio/src/main/java/com/example/cardapio/food/FoodRepository.java
+++ cardapio/src/main/java/com/example/cardapio/food/FoodRepository.java
@@ -3,7 +3,8 @@ package com.example.cardapio.food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
-
+    
+    public Food findById(Integer id);
     public Food findByTitle(String title);
 
 }
